bc858d3295f3112c218877e51d907bcc2bccc380
khAayush <khatiwadaprince1@gmail.com>
2025-11-09 11:36:41 +0545

    Initialized Backend

---
+++ Backend/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening to requests on port: ${process.env.PORT}`);
+});
