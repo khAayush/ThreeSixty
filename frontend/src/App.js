590b543a7e2eea589fa33afe8987e8d6da182caf
khAayush <khatiwadaprince1@gmail.com>
2025-11-15 23:34:44 +0545

    Added Landing page Features Section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import FeatureSection from './components/FeatureSection';
 
 function App() {
   return (
-    <><Navbar /><Hero /></>
+    <>
+    <Navbar />
+    <Hero />
+    <FeatureSection />
+    </>
   );
 }
 
