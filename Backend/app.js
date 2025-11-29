d11c2203c30916b1b41e926e46bcd1a854007379
khAayush <khatiwadaprince1@gmail.com>
2025-11-29 20:17:35 +0545

    Database Configuration

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config();
 const app = express();
 
 app.use(express.json());
+
+// Connect to MongoDB
+connectDB();
 
 app.listen(process.env.PORT, () => {
     console.log(`Listening to requests on port: ${process.env.PORT}`);
